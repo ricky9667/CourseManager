e6c6aac1a89e782177e16cd76b9aa2f61e74aed5
Ricky Hu <ricky9667@gmail.com>
2021-10-14 13:28:38 +0800

    load course data grid view in all tabs

--- CourseManager/CourseSelectingForm.cs
+++ CourseManager/CourseSelectingForm.cs
@@ -7,6 +7,7 @@ namespace CourseManager
     public partial class CourseSelectingForm : Form
     {
         CourseModel _courseModel;
+        List<CourseTabPageInfo> _courseTabPageInfos;
 
         public CourseSelectingForm(CourseModel courseModel)
         {
@@ -17,20 +18,51 @@ namespace CourseManager
         // load select course form
         private void CourseSelectingFormLoad(object sender, EventArgs e)
         {
-            List<CourseTabPageInfo> courseTabPageInfos = _courseModel.GetCourseTabPageInfos();
-            int tabCount = courseTabPageInfos.Count;
-
+            _courseTabPageInfos = _courseModel.GetCourseTabPageInfos();
+            int tabCount = _courseTabPageInfos.Count;
             for (int index = 0; index < tabCount; index++)
             {
-                CourseTabPageInfo tabPageInfo = courseTabPageInfos[index];
+                CourseTabPageInfo tabPageInfo = _courseTabPageInfos[index];
                 courseTabControl.Controls[index].Name = tabPageInfo.TabName;
                 courseTabControl.Controls[index].Text = tabPageInfo.TabText;
-                
-                //TabPage courseTabPage = GetCourseTabPage(tabPageInfo.TabName, tabPageInfo.TabText, index, tabPageInfo.CourseLink);
-                //courseTabControl.Controls.Add(courseTabPage);
             }
 
-            courseDataGridView.DataSource = _courseModel.GetCourseInfos(courseTabPageInfos[0].CourseLink);
+            SetUpCourseDataGridView();
+        }
+
+        // setup data grid view
+        private void SetUpCourseDataGridView()
+        {
+            courseDataGridView.DataSource = _courseModel.GetCourseInfos(_courseTabPageInfos[0].CourseLink);
+            SetUpCourseGridViewColumns();
+            courseDataGridView.Columns.Insert(0, new DataGridViewCheckBoxColumn
+            {
+                HeaderText = "選",
+                Width = 30,
+                ReadOnly = false,
+                TrueValue = true,
+                FalseValue = false
+            });
+        }
+
+        // rename header text of datagridview
+        private void SetUpCourseGridViewColumns()
+        {
+            Console.WriteLine(courseDataGridView.Columns.Count);
+
+            int columnCount = 23;
+            string[] columnHeaderText = { "number", "name", "stage", "credit", "hour", "courseType", "teacher", "classtime0", "classtime1", "classtime2", "classtime3", "classtime4", "classtime5", "classtime6", "classroom", "numberOfStudent", "numberOfDropStudent", "teachingAssistant", "language", "note", "outline", "audit", "experiment" };
+            string[] columnHeaderTextChinese = { "課號", "課程名稱", "階段", "學分", "時數", "修", "教師", "日", "一", "二", "三", "四", "五", "六", "教室", "人", "撤", "教學助理", "授課語言", "教學大綱與進度表", "備註", "隨班附讀", "實驗實習" };
+
+            for (int index = 0; index < columnCount; index++)
+            {
+                string headerText = columnHeaderText[index];
+                string headerTextChinese = columnHeaderTextChinese[index];
+                Console.WriteLine(index + " : " + headerText + " : " + headerTextChinese);
+
+                courseDataGridView.Columns[headerText].HeaderText = headerTextChinese;
+                Console.WriteLine("headerText: " + courseDataGridView.Columns[headerText]);
+            }
         }
 
         // dynamically create course tab page 
@@ -92,9 +124,12 @@ namespace CourseManager
             }
         }
 
+        // add datagridview to selected tab index
         void CourseTabControlSelectedIndexChanged(object sender, EventArgs e)
         {
-            Console.WriteLine("Tab index changed");
+            int index = courseTabControl.SelectedIndex;
+            courseDataGridView.DataSource = _courseModel.GetCourseInfos(_courseTabPageInfos[index].CourseLink);
+            courseTabControl.Controls[index].Controls.Add(courseDataGridView);
         }
     }
 }
