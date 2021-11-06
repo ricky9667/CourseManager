409994c7eb6558602694a85bb5687aa9e165e9dd
Ricky Hu <ricky9667@gmail.com>
2021-11-06 16:19:19 +0800

    add unit tests to startup form view model

---
+++ CourseManagerTests/ViewModels/StartUpFormViewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CourseManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Tests
+{
+    [TestClass()]
+    public class StartUpFormViewModelTests
+    {
+        Model model;
+        StartUpFormViewModel viewModel;
+        [TestInitialize]
+        public void Setup()
+        {
+            model = new Model();
+            viewModel = new StartUpFormViewModel(model);
+        }
+
+        [TestMethod()]
+        public void StartUpFormViewModelTest()
+        {
+            Assert.IsTrue(viewModel.CourseSelectingSystemButtonEnabled);
+            Assert.IsTrue(viewModel.CourseManagementSystemButtonEnabled);
+        }
+
+        [TestMethod()]
+        public void ControlsEnabledTest()
+        {
+            viewModel.CourseSelectingSystemButtonEnabled = false;
+            Assert.IsFalse(viewModel.CourseSelectingSystemButtonEnabled);
+            viewModel.CourseManagementSystemButtonEnabled = false;
+            Assert.IsFalse(viewModel.CourseManagementSystemButtonEnabled);
+        }
+    }
+}
